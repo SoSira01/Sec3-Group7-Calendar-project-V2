29a83c2095d7438952343f80c93719298f91fadd
so <S.phairoh20dec@gmail.com>
2022-04-16 15:56:22 +0700

    mix css

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,39 @@
 <script setup></script>
 
 <template>
-  <div>
-    <div>
-      <router-link :to="{ name: 'Home' }">Home</router-link>
-      <router-link :to="{ name: 'Notes' }">Notes</router-link>
-      <router-link :to="{ name: 'AboutUs' }">AboutUs</router-link>
-    </div>
+
+  <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAAXNSR0IArs4c6QAAAKdJREFUSEtjZKAxYKSx+QyELPgPdQAudYTk6W+BAwMDw3wGBgYFMoPuAQMDQyIDA8MBmH50r4MUyJNpOEwbyAxFXBbAwpRCOxBBj+4DgpFGwGYM/QNmAc2DaPhYAIsj9MjDxcdInbgiGV0hzSwgNy7gDifkA5pbMBoHGPUBzQu7DwwMDPzkxixU30Pk+gQ9FYEqnAUU1AkgwxPwVTgUOh5TO6FKn2ILAT6uKhlJWfgFAAAAAElFTkSuQmCC"/>
+  <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+ <h2>Calendar</h2>
+  </a>
+ 
+
+      <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+           <router-link :to="{ name: 'Home' }">Home</router-link>
+
+      </a>
+      <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+            <router-link :to="{ name: 'Notes' }">Notes</router-link>
+
+      </a>
+      <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white">
+            <router-link :to="{ name: 'AboutUs' }">AboutUs</router-link>
+
+      </a>
+
+<!-- 
+    <router-link :to="{ name: 'Home' }">Calendar</router-link>
+    <router-link :to="{ name: 'Notes' }">Notes</router-link>
+    <router-link :to="{ name: 'AboutUs' }">AboutUs</router-link> -->
+
     <div>
       <router-view></router-view>
     </div>
-  </div>
 </template>
 
 <style scoped>
 a{
-  color: gray
+  color: rgb(255, 255, 255)
 }
 .router-link-active {
   color:blue
